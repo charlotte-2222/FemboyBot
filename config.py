b73c541a6ac2923453bea58e0d8f4cf1b4e65d0c
Zach Childers <67248738+im-zach@users.noreply.github.com>
2021-03-04 15:58:33 -0500

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-token = "NjgwNTI5NDU0MjAzNjY2NDc5.XlBOcQ.LdVfMyEeft98U6LLflzui80M9pE"
-imgurC = "94ecee09fffc2a0"
-ImgurL = "dbac397e0b59bbbfb00061272b3153069ca96941"
-redditC = "GRb11xHgsNfBRA"
-redditCS="_CSXhXlATgjZ0r5mcXoLuYGV8nTOsA"
-redditU_A="-flop"
