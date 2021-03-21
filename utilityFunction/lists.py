2652d9235909e4ef3b0f8b34ddbd5d5731391e39
Zachary Childers <zdchilders10@sccsc.edu>
2021-03-21 10:52:47 -0400

    A few minor updates and command additions: - rate a fellow user's dick length - baka - Increased reddit commands post_to_pick (1,45), rather than .hot() posts, now scrapes at random. [This is a test feature on NSFW related commands based on client analytics]
    - Additional 8ball responses

--- utilityFunction/lists.py
+++ utilityFunction/lists.py
@@ -4,5 +4,13 @@ ballresponse = [
     "You'll be the judge", 'no... (╯°□°）╯︵ ┻━┻',
     'senpai, pls no ;-;', 'no and if you ask again i will cum on you',
     "*cums*\nyes", 'why would you ask such a bold question.\nthe answer is no',
-    'ask flop', 'ask Grim', 'if you stop simpin, maybe'
+    'ask flop', 'ask Grim', 'if you stop simpin, maybe', "what a stupid question",
+    "hear me out.\nroll a D20. The number you get is the amount of people you have"
+    "to scarifice to Luke Smith.\nKidnap those people. Take them to Bungie Headquarters\n"
+    "Eat them.\nOr don't, up to you i guess.",
+    "*psst... i'm a bowling ball*",
+    "i guess i dunno",
+    "suck my elbow first, then maybe you can",
+    "Sure"
+    "mmmmmm. nnnnnyes"
 ]
