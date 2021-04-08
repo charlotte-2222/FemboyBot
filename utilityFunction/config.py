05ba132c06212d94b9497428cf0e49bf251e60ff
Zachary Childers <zdchilders10@sccsc.edu>
2021-04-08 00:46:34 -0400

    Update on past ~week

--- utilityFunction/config.py
+++ utilityFunction/config.py
@@ -1,11 +1,12 @@
 token = ""
 imgurC = "94ecee09fffc2a0"
 ImgurL = "dbac397e0b59bbbfb00061272b3153069ca96941"
-redditC = "GRb11xHgsNfBRA"
-redditCS = "_CSXhXlATgjZ0r5mcXoLuYGV8nTOsA"
-redditU_A = "-flop"
-OWNERS=[384459643545583627]
-BLACKLIST=[]
+redditC="GRb11xHgsNfBRA"
+redditCS="_CSXhXlATgjZ0r5mcXoLuYGV8nTOsA"
+redditU_A="-flop"
+prefix="^"
+BLACKLIST = []
+OWNERS = [384459643545583627]
 HOMIE_ROLE = 694714808150261780
 BOT_ROLE = 694710377128132628
 
